32efa3024d35d8c987548f943727e0f346eaef5d
Henok Asaye <henokasaye77@gmail.com>
2025-01-06 09:36:20 +0300

    add a function to serve the static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { JwtAuthMiddleware } from './auth/middleware/auth.middleware';
 import { JwtModule } from '@nestjs/jwt';
-import {AuthModule} from "./auth/auth.module"
+import { AuthModule } from "./auth/auth.module";
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +9,8 @@ import { PatientModule } from './patient/patient.module';
 import { AdminModule } from './admin/admin.module';
 import { DoctorModule } from './doctor/doctor.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static'; // Add this import
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -20,8 +22,12 @@ import { PrismaModule } from '../prisma/prisma.module';
     AdminModule,
     DoctorModule,
     PrismaModule,
-    ConfigModule
-
+    ConfigModule,
+    
+    // Serve static files from the 'public' folder in the frontend
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'Frontend', 'public'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
@@ -29,19 +35,17 @@ import { PrismaModule } from '../prisma/prisma.module';
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-  .apply(JwtAuthMiddleware)
-  .exclude(
-    { path: '/auth/login', method: RequestMethod.POST },
-    { path: '/auth/signup', method: RequestMethod.POST },
-    { path: '/auth/signupdoctor', method: RequestMethod.POST },
-    { path: '/auth/firstadmin', method: RequestMethod.POST },
-    { path: '/auth/loginadmin', method: RequestMethod.POST },
-    { path: '/index.html', method: RequestMethod.GET }, // Exclude index.html
-    { path: '/', method: RequestMethod.GET }, // Exclude root
-    { path: '(.*)', method: RequestMethod.GET } // Exclude all static files
-  )
-  .forRoutes('*');
+      .apply(JwtAuthMiddleware)
+      .exclude(
+        { path: '/auth/login', method: RequestMethod.POST },
+        { path: '/auth/signup', method: RequestMethod.POST },
+        { path: '/auth/signupdoctor', method: RequestMethod.POST },
+        { path: '/auth/firstadmin', method: RequestMethod.POST },
+        { path: '/auth/loginadmin', method: RequestMethod.POST },
+        { path: '/index.html', method: RequestMethod.GET }, // Exclude index.html
+        { path: '/', method: RequestMethod.GET }, // Exclude root
+        { path: '(.*)', method: RequestMethod.GET } // Exclude all static files
+      )
+      .forRoutes('*');
   }
 }
-
-
